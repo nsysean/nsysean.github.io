bcf3a714b926d5a37b269d621b43b9b12c80b248
Nicholas Ly <hello@nicholasly.com>
2024-11-06 13:56:15 -0600

    feat: layout and page designs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+  site: SITE_URL,
+  integrations: [mdx(), sitemap(), tailwind()],
 });
